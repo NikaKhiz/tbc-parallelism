2ab413a9b5578843e064ab52ddf1cf32dbcc5128
NikaKhiz <khizambarelinika123@gmail.com>
2024-09-15 00:16:08 +0400

    refactor fetch and write isntead of using queues write fetched content directly in to the file

--- main.py
+++ main.py
@@ -1,46 +1,53 @@
 import requests, json, threading, time
-from queue import Queue
+from concurrent.futures import ThreadPoolExecutor
 
 url = "https://jsonplaceholder.typicode.com/posts/{}"
 num_of_posts = 77
-posts = []
 lock = threading.Lock()
+num_of_posts_written = 1
 
 def main():
     start_time = time.perf_counter()
     
     fetch_posts_using_threads()
-    write_to_a_file('posts.json', posts)
     
     end_time = time.perf_counter()
     print(f"Time taken: {end_time - start_time:.2f} seconds")
 
 
-# fetch post from url according to given post id 
-def fetch_post(post_id):
+# fetch post from url according to given post id and write it directly in to the file 
+def fetch_and_write(post_id):
+    global num_of_posts_written
+
     try:
         response = requests.get(url.format(post_id))
         with lock:
-            posts.append(response.json())
+            with open('posts.json', 'a') as file:
+                json.dump(response.json(), file, indent=4)
+
+                # increase number of posts written
+                # to append necessary characters in to the file
+                num_of_posts_written += 1
+                
+                if num_of_posts_written <= num_of_posts:
+                    file.write(',\n')
+                else:
+                    file.write('\n]')
+
     except requests.RequestException as e:
         print(f"Error fetching post {post_id}: {e}")
 
 
-# use threading for time efficiency
+# truncate file content and set its initial state
+# use threadpoolexecutor for fetching multiple posts simultaneously
 def fetch_posts_using_threads():
-    threads = []
-    for post_id in range(1, num_of_posts + 1):
-        thread = threading.Thread(target=fetch_post, args=(post_id,))
-        thread.start()
-        threads.append(thread)
-    
-    for thread in threads:
-        thread.join()
+    with open('posts.json', 'a') as file:
+            file.seek(0)
+            file.truncate()
+            file.write('[\n')
 
-# write data posts list in to the file
-def write_to_a_file(filename, posts):
-        with open(filename, 'w') as file:
-            json.dump(posts, file, indent=4)
+    with ThreadPoolExecutor(max_workers=15) as executor:
+        executor.map(fetch_and_write, range(1, num_of_posts + 1))
 
 
 if __name__ == "__main__":
